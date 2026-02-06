0e55a42c4faa2d2b1d19860566bd283e29ba85ae
Eshwar-21at1a3529 <erao2303@gmail.com>
2026-02-07 01:14:33 +0530

    Fix: attach JWT token to API requests

--- client/src/api.js
+++ client/src/api.js
@@ -4,4 +4,12 @@ const API = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
 });
 
+API.interceptors.request.use((config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+        config.headers["x-auth-token"] = token;
+    }
+    return config;
+});
+
 export default API;
